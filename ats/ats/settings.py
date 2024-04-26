dba1f710f6cac3b5a21eca750b6fd2a8a9bf97eb
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2024-04-27 00:58:37 +0900

    feat: settings.py에 media 추가

--- ats/ats/settings.py
+++ ats/ats/settings.py
@@ -119,12 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_staticfiles')
 STATICFILES_DIRS = [
     'myapp/static',  # Assumed path where your static files are currently located
 ]
-STATIC_ROOT = 'collected_staticfiles'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
