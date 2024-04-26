4810d6a83515c845b99ba205fa04da4bcfbc76f2
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2024-04-26 11:42:47 +0000

    240426 1차 배포본 : 전체랭킹, 전체기록

--- ats/ats/settings.py
+++ ats/ats/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-$%o3xie_=ro-&l*ydwi+$kcemu24t2#!aeuqopp)bjn+m_za@_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djagpdud125.pythonanywhere.com']
 
 
 # Application definition
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    'myapp/static',  # Assumed path where your static files are currently located
+]
+STATIC_ROOT = 'collected_staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
